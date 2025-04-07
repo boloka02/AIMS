e8e80d3419c2480e6df61c1eb9e7ea42d3e48da1
Elton Rey Ybanez <boloka02@gmail.com>
2025-04-07 15:46:14 +0800

    update

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const mysql = require('mysql');  // Only need this line once
+const mysql = require('mysql');
 const cors = require('cors');
-const fs = require('fs');  // Add this line to include fs module
 
 // Initialize the app
 const app = express();
@@ -11,14 +10,13 @@ app.use(cors());
 
 // Database connection
 const db = mysql.createConnection({
-  host: '34.101.76.100',
+  host: '10.184.0.2',
   user: 'root',
   password: 'T0pSecret@2025!',
   database: 'db_ams',
   port: 3306,
-  connectTimeout: 10000  // Set to 10 seconds
+  connectTimeout: 10000
 });
-
 
 // Connect to the database
 db.connect((err) => {
@@ -31,10 +29,9 @@ db.connect((err) => {
 
 // Set up a basic route
 app.get('/', (req, res) => {
-  console.log('Received request at /');
+  console.log('Received request at /');  // Ensure this log is printed
   res.send('AIMS Application is running');
 });
-
 
 // Start the server on port 3000
 app.listen(3000, () => {

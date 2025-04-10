8b05acd5b12f81606f3ea6b84cd8f6feccf3ae33
Elton Rey Ybanez <boloka02@gmail.com>
2025-04-10 11:18:06 +0800

    updated

--- dashboard/ticketstatus.php
+++ dashboard/ticketstatus.php
@@ -1,5 +1,13 @@
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php include "../sidebar/sidebar.php"; ?>
+    
 <div class="card">
     <div class="card-body">
         <h5 class="card-title">Ticket Status</h5>
@@ -43,3 +51,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 </script>
+</body>
+</html>
+

ba93e890de079a8d0655de6f658908ac9c2e1f5a
Elton Rey Ybanez <boloka02@gmail.com>
2025-04-10 10:44:39 +0800

    up

--- dashboard/ticketstatus.php
+++ dashboard/ticketstatus.php
@@ -1,16 +1,8 @@
 
-        <!-- Ticket Status (Pie Chart) -->
-        <div class="col-lg-6">
-            <div class="card shadow-sm">
-                <div class="card-body">
+ 
                     <h5 class="card-title">Ticket Status</h5>
-                    <div style="height: 300px;">
                         <canvas id="statusChart"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+           
 
  <script>
 document.addEventListener("DOMContentLoaded", function () {

85eab6fc350f22f8675fa66c490b40e9a5716baa
Elton Rey Ybanez <boloka02@gmail.com>
2025-04-10 10:23:47 +0800

    aded

---
+++ dashboard/ticketcategory.php
@@ -0,0 +1,15 @@
+<div class="col-lg-6">
+        <div class="card shadow-sm">
+            <div class="card-body d-flex justify-content-between align-items-center">
+                <h5 class="card-title">Ticket Categories</h5>
+                <button class="btn btn-primary">View Category</button>
+            </div>
+            <div class="card-body">
+                <div style="height: 300px;">
+                    <canvas id="categoryChart"></canvas>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="pi.js"></script>

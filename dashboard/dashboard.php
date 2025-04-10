ae2605994f85a020709e6c9a779d5c5d175cd71d
Elton Rey Ybanez <boloka02@gmail.com>
2025-04-10 10:06:05 +0800

    up

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -141,7 +141,7 @@ mysqli_close($conn);
 </div>
 
 
-    <div class="row mt-3">
+    <div class="row mt-2">
     <!-- Recent Activity (Smaller Box, Left Side) -->
     <div class="col-lg-6"> <!-- Reduced width from col-lg-6 to col-lg-4 -->
         <div class="card shadow-sm h-100" style="max-height: 2500px;"> <!-- Reduced height -->
